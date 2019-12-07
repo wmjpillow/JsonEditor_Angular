35404b69f62c9330fbd01e498c80442501d01dc6
wmjpillow <mwang3@wpi.edu>
2019-12-07 13:08:07 -0500

    adjust UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,87 +10,84 @@ import { HttpClient } from "@angular/common/http";
 })
 
 export class AppComponent implements OnInit {
-    dialog: MatDialog;
-    renderer: Renderer2;
-    options: any;
-    jsonEditorCode: any;
-    jsonEditorTree: any;
-    autoConvert: boolean;
-    jsonCode: any;
-    products: any = [];
+    // dialog: MatDialog;
+    // renderer: Renderer2;
+    // options: any;
+    // jsonEditorCode: any;
+    // jsonEditorTree: any;
+    // autoConvert: boolean;
+    // jsonCode: any;
+    // products: any = [];
 
-    constructor(dialog: MatDialog, renderer: Renderer2, private httpClient: HttpClient) {
-        this.dialog = dialog;
-        this.renderer = renderer;
-
+    constructor() {
     }
 
     ngOnInit() {
-        this.options = {
-            code : {
-                mode: 'code',
-                onChange: () => {
-                    const json = this.jsonEditorCode.get();
-                    if (json) {
-                        this.jsonCode = json;
-                        this.setLocalStorage('jsonCode', JSON.stringify(json));
-                        // if (this.autoConvert) {
-                            this.validateJSON('Tree');
-                        // }
-                    }
-                }
-            },
-            tree : {
-                mode: 'tree',
-                onChange: () => {
-                    const json = this.jsonEditorTree.get();
-                    if (json) {
-                        this.jsonCode = json;
-                        this.setLocalStorage('jsonCode', JSON.stringify(json));
-                        this.validateJSON('Code');
-                    }
-                }
-            }
-        };
-        this.jsonEditorCode = new JSONEditor(document.getElementById('jsonEditorCode'), this.options.code);
-        this.jsonEditorTree = new JSONEditor(document.getElementById('jsonEditorTree'), this.options.tree);
-        // this.setDefaultOptions();
-        this.httpClient.get("assets/json/admin.json").subscribe(data =>{
-            data;
-            console.log(data);
-            this.autoConvert = JSON.parse(localStorage.getItem('autoConvertJSON'));
-            this.jsonCode = localStorage.getItem('jsonCode') ? JSON.parse(localStorage.getItem('jsonCode')) : data;
-            this.validateJSON('Tree');
-            // if (this.autoConvert) {
-                this.validateJSON('Code');
-            // }
-          })
+    //     this.options = {
+    //         code : {
+    //             mode: 'code',
+    //             onChange: () => {
+    //                 const json = this.jsonEditorCode.get();
+    //                 if (json) {
+    //                     this.jsonCode = json;
+    //                     this.setLocalStorage('jsonCode', JSON.stringify(json));
+    //                     // if (this.autoConvert) {
+    //                         this.validateJSON('Tree');
+    //                     // }
+    //                 }
+    //             }
+    //         },
+    //         tree : {
+    //             mode: 'tree',
+    //             onChange: () => {
+    //                 const json = this.jsonEditorTree.get();
+    //                 if (json) {
+    //                     this.jsonCode = json;
+    //                     this.setLocalStorage('jsonCode', JSON.stringify(json));
+    //                     this.validateJSON('Code');
+    //                 }
+    //             }
+    //         }
+    //     };
+    //     this.jsonEditorCode = new JSONEditor(document.getElementById('jsonEditorCode'), this.options.code);
+    //     this.jsonEditorTree = new JSONEditor(document.getElementById('jsonEditorTree'), this.options.tree);
+    //     // this.setDefaultOptions();
+    //     this.httpClient.get("assets/json/admin.json").subscribe(data =>{
+    //         data;
+    //         console.log(data);
+    //         this.autoConvert = JSON.parse(localStorage.getItem('autoConvertJSON'));
+    //         this.jsonCode = localStorage.getItem('jsonCode') ? JSON.parse(localStorage.getItem('jsonCode')) : data;
+    //         this.validateJSON('Tree');
+    //         // if (this.autoConvert) {
+    //             this.validateJSON('Code');
+    //         // }
+    //       })
   
-    }
+    // }
 
-    validateJSON = (type) => {
-        if (type === 'Tree') {
-            this.jsonEditorTree.set(this.jsonCode);
-        } else if (type === 'Code') {
-            this.jsonEditorCode.set(this.jsonCode);
-        }
-    }
+    // validateJSON = (type) => {
+    //     if (type === 'Tree') {
+    //         this.jsonEditorTree.set(this.jsonCode);
+    //     } else if (type === 'Code') {
+    //         this.jsonEditorCode.set(this.jsonCode);
+    //     }
+    // }
 
-    setLocalStorage = (key, value) => {
-        localStorage.setItem(key, value);
-    }
+    // setLocalStorage = (key, value) => {
+    //     localStorage.setItem(key, value);
+    // }
 
-    clearStorageOptions = () => {
-        const dialogRef = this.dialog.open(ConfirmResetComponent, {
-            restoreFocus: false
-        });
+    // clearStorageOptions = () => {
+    //     const dialogRef = this.dialog.open(ConfirmResetComponent, {
+    //         restoreFocus: false
+    //     });
 
-        dialogRef.afterClosed().subscribe((result) => {
-            if (result) {
-                localStorage.removeItem('jsonCode');
-                localStorage.removeItem('autoConvertJSON');
-                // this.setDefaultOptions();
-            }
-        });
+    //     dialogRef.afterClosed().subscribe((result) => {
+    //         if (result) {
+    //             localStorage.removeItem('jsonCode');
+    //             localStorage.removeItem('autoConvertJSON');
+    //             // this.setDefaultOptions();
+    //         }
+    //     });
     }
 }
--- src/app/json-editor/json-editor.component.ts
+++ src/app/json-editor/json-editor.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import * as JSONEditor from 'jsoneditor';
+import { ConfirmResetComponent } from '../confirm-reset/confirm-reset.component';
+import { MatDialog } from '@angular/material/dialog';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-json-editor',
@@ -6,10 +10,86 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./json-editor.component.css']
 })
 export class JsonEditorComponent implements OnInit {
+  dialog: MatDialog;
+  renderer: Renderer2;
+  options: any;
+  jsonEditorCode: any;
+  jsonEditorTree: any;
+  autoConvert: boolean;
+  jsonCode: any;
+  products: any = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(dialog: MatDialog, renderer: Renderer2, private httpClient: HttpClient) { 
+    this.dialog = dialog;
+    this.renderer = renderer;
   }
 
+  ngOnInit() {
+        this.options = {
+            code : {
+                mode: 'code',
+                onChange: () => {
+                    const json = this.jsonEditorCode.get();
+                    if (json) {
+                        this.jsonCode = json;
+                        this.setLocalStorage('jsonCode', JSON.stringify(json));
+                        // if (this.autoConvert) {
+                            this.validateJSON('Tree');
+                        // }
+                    }
+                }
+            },
+            tree : {
+                mode: 'tree',
+                onChange: () => {
+                    const json = this.jsonEditorTree.get();
+                    if (json) {
+                        this.jsonCode = json;
+                        this.setLocalStorage('jsonCode', JSON.stringify(json));
+                        this.validateJSON('Code');
+                    }
+                }
+            }
+        };
+        this.jsonEditorCode = new JSONEditor(document.getElementById('jsonEditorCode'), this.options.code);
+        this.jsonEditorTree = new JSONEditor(document.getElementById('jsonEditorTree'), this.options.tree);
+        // this.setDefaultOptions();
+        this.httpClient.get("assets/json/admin.json").subscribe(data =>{
+            data;
+            console.log(data);
+            this.autoConvert = JSON.parse(localStorage.getItem('autoConvertJSON'));
+            this.jsonCode = localStorage.getItem('jsonCode') ? JSON.parse(localStorage.getItem('jsonCode')) : data;
+            this.validateJSON('Tree');
+            // if (this.autoConvert) {
+                this.validateJSON('Code');
+            // }
+          })
+  
+    }
+
+    validateJSON = (type) => {
+        if (type === 'Tree') {
+            this.jsonEditorTree.set(this.jsonCode);
+        } else if (type === 'Code') {
+            this.jsonEditorCode.set(this.jsonCode);
+        }
+    }
+
+    setLocalStorage = (key, value) => {
+        localStorage.setItem(key, value);
+    }
+
+    clearStorageOptions = () => {
+        const dialogRef = this.dialog.open(ConfirmResetComponent, {
+            restoreFocus: false
+        });
+
+        dialogRef.afterClosed().subscribe((result) => {
+            if (result) {
+                localStorage.removeItem('jsonCode');
+                localStorage.removeItem('autoConvertJSON');
+                // this.setDefaultOptions();
+            }
+        });
+    }
 }
